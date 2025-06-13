bb831effe2fb840c80f21bf0f3d57c9afe8224b1
njomzabehadini23 <nb30990@seeu.edu.mk>
2025-06-13 23:07:27 +0200

    Add the notification service

---
+++ notification_service/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/notify", methods=["POST"])
+def notify():
+    data = request.json
+    print(f"🔔 Notification: {data.get('message')}")
+    return {"notified": True}, 200
+
+if __name__ == "__main__":
+    app.run(port=5003)
